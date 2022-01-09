88831d91ac18173dba73412f7384a75d55e07f5c
Юрий Дембицкий <y.d.asutp@gmail.com>
2022-01-09 20:51:45 +0700

    ex19 ready

---
+++ Ex19_MVVM/AppRadius/Models/Arith.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppRadius.Models
+{
+    static class Arith
+    {
+        public static double LengthCircle(int radius)
+        {
+            double lengthCircle = 2 * Math.PI * radius;
+            return lengthCircle;
+        }
+    }
+}
